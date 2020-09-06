40323cbfb43fc86bb35e2d07771f51ea7d116d6c
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 03:38:44 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,5 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, value|
-    if key == suite_a && value.start_with?
+    if key == suite_a && value.start_with?("A")
+      print value
+    end
 end
